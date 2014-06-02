b2a083f39be473bba7c3f076e37c4ff5ca3533f5
dvb-adssets <dennis.vonbargen@adssets.com>
2014-06-02 14:08:52 +0200

    changes made to T1

--- src/hellothread/T1.java
+++ src/hellothread/T1.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package hellothread;
 
 /**
@@ -11,31 +10,30 @@ package hellothread;
  * @author Baronen
  */
 public class T1 implements Runnable {
-    
+
     boolean active;
-    
+
     Thread t;
-    
+
     public T1() {
         active = true;
         t = new Thread(this);
         t.start();
     }
-    
+
+    private void printSecond(String message) {
+        System.out.println(message);
+        try {
+            t.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public void run() {
-        while(active) {
-            System.out.println("Tick...");
-            try {
-                t.sleep(1000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("Tock...");
-            try {
-                t.sleep(1000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
+        while (active) {
+            printSecond("Tick...");
+            printSecond("Tack...");
         }
     }
 }
