661bddc63e82b11eac48f7d985b473346cd4ded1
dvb-adssets <dennis.vonbargen@adssets.com>
2014-06-03 13:14:00 +0200

    Oh no, malicious code included!

--- src/hellothread/T2.java
+++ src/hellothread/T2.java
@@ -67,9 +67,17 @@ public class T2 extends Thread {
             tippTapp();
             tippe();
             tippe();
+            smoer(); // I snuck in here, muahahah!
             tippTapp();
             endSong();
             pauseSec();
+        }
+    }
+    
+    // Hi, I'm a malicious method. I print "smör" all the time.
+    public void smoer() {
+        while(true) {
+            System.out.println("Smör");
         }
     }
 }
